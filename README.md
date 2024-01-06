82d77b4aec2526c9bfe3d869f41c86b80e95527d
drxxstrange@gmail.com <drxxstrange@gmail.com>
2024-01-06 22:32:07 +0530

    V2

--- README.md
+++ README.md
@@ -1,36 +1,153 @@
 ## TgBotStatus
 A Simple Telegram Status Updater to Pretify All your Bots for your Channel in single Status..
 
-## ***Deploy***
-- Only Deployable on Workflows
-- Fork & Star and Enable `Actions` -> `Run Workflow`
+---
 
 ## ***Features***
-- Sequential Status Check
-- Progress on Checking 
-- Calculate Ping Time
-- Available Bots
-- Fresh Clean UI
-- Set Custom Hosts for Each Bots
+- _Sequential Status Check_
+- _Progress Bar on Checking_
+- _Calculate Ping Time of Response_
+- _Show Available/Working Bots_
+- _Fresh Clean UI_
+- _Set Custom Host for Each Bot(s)_
+- _Support for MLTB Advanced Status (More Status Details)_
 
-## ***Config SetUp***:
+---
+
+## ***Repo Config SetUp***:
+
+### _`.env` File SetUp :_
 - `APP_ID`: Authenticate your Telegram account, get this from https://my.telegram.org.
 - `API_HASH`: Authenticate your Telegram account, get this from https://my.telegram.org.
-- `SESSION`: Telethon User Session to Access Bots, Generate from [Here](https://colab.research.google.com/drive/1wjYvtwUo5zDsUvukyafAR9Of-2NYkKsu)
-- `BOTS`: Username of Bots Separated by single Space ( without @ )
-  > bot01Bot bot02Bot
-- `HOSTS`: Hosts corresponding to Bots List Var separated by single Space
-  > host host host2
-- `CHANNEL_ID`: Channel ID of the Message in which Msg ID is to be Edited
-- `MESSAGE_ID`: Message ID of the Message to Edit
-- `CHANNEL_NAME`: Any Channel Description or Just Name
+- `PYRO_SESSION`: Pyrogram User Session to Access Bots, Generate from [Here](https://colab.research.google.com/drive/1wjYvtwUo5zDsUvukyafAR9Of-2NYkKsu)
+- `HEADER_MSG` : Put Header Msg for 1st Line. 
+  > Default: Telegram Bot Status :
 - `TIME_ZONE`: Time Zone for Sync with your Local Time
+  > Default: Asia/Kolkata
+
+### _`config.json` File SetUp :_
+- _Sections are Divided into 2 Parts_:
+  1. Bots Details:
+    `bot1`: Indentifier Name (Can be Anything But Unique for Every Bot)
+
+    |Variable|Value|Required|
+    |:---:|:---:|:---:|
+    |`base_url_of_bot`|If MLTB bot, give Base URL of it.|(Optional)|
+    |`host`|Host name where you have deployed|*Required|
+    |`bot_uname`|Bot Username without @|*Required|
+    
+  2. Chat Details:
+    `chat1`: Indentifier Name (Can be Anything But Unique for Every Bot)
+
+    |Variable|Value|Required|
+    |:---:|:---:|:---:|
+    |`chat_id`|chat id of the Target Channel or Group|*Required|
+    |`message_id`|message id of the Message to Edit. If link is https://t.me/cha_uname/123 Here, 123 is the Message ID|*Required|
+
+#### Sample JSON Format
+```json
+{
+  "bots": {
+    "bot1": {
+      "base_url_of_bot": "http://0.0.0.0",
+      "host": "HK",
+      "bot_uname": "@botfather"
+    },
+    "bot2": {
+      "host": "Vps",
+      "bot_uname": "@botfather"
+    }
+    ...more
+  },
+  "channels": {
+    "chat1": {
+      "chat_id": "-100987654321",
+      "message_id": "54321"
+    },
+    "chat2": {
+      "chat_id": "-100123456789",
+      "message_id": "12345"
+    }
+    ...more
+  }
+}
+```
+
+### _Required Config Setup :_
+Either Add these URL to these Variables or Directly Add a File on Repo as File name Specified.
+
+- `CONFIG_ENV_URL`: _(Optional if .env provided)_ Direct URL of `.env` file posted on [gist.github.com](https://gist.github.com)
+- `CONFIG_JSON_URL`:  _(Optional if config.json provided)_ Direct URL of `config.json` file posted on [gist.github.com](https://gist.github.com)
+
+> NOTE: CONFIG_JSON_URL & CONFIG_ENV_URL will overwrite the existing files if provided.
+
+---
+
+## ***Deploy Guide***
+- Only Deployable on Workflows
+- Soon Add for Heroku & VPS Users
+
+### _Prerequisites:_
+- Setup `config.json` and `.env`
+- Send a Dummy Message on the Channel (say 'test') you want to Setup Status and Retrieve the message id of it.
+
+### _Procedure:_
+- **Step 1:** _Fork & Star the Repo_
+- **Step 2:** _Set Variables in Secrets in Settings Tab_
+  > Available Variables: APP_ID, API_HASH, PYRO_SESSION, CONFIG_ENV_URL, CONFIG_JSON_URL
+- **Step 3:** _Enable `Actions` -> `Select Workflow` -> `Run Workflow`_
+
+---
+
+## ***Advanced MLTB Status***:
+_Set the Code in the Required File `wserver.py` at the Last_
+**Path :** ./web/wserver.py 
+
+```py
+from time import sleep, time
+from psutil import boot_time, disk_usage, net_io_counters
+from subprocess import check_output
+from os import path as ospath
+
+botStartTime = time()
+if ospath.exists('.git'):
+    commit_date = check_output(["git log -1 --date=format:'%y/%m/%d %H:%M' --pretty=format:'%cd'"], shell=True).decode()
+else:
+    commit_date = 'No UPSTREAM_REPO'
+
+@app.route('/status', methods=['GET'])
+def status():
+    bot_uptime = time() - botStartTime
+    uptime = time() - boot_time()
+    sent = net_io_counters().bytes_sent
+    recv = net_io_counters().bytes_recv
+    return {
+        'commit_date': commit_date,
+        'uptime': uptime,
+        'on_time': bot_uptime,
+        'free_disk': disk_usage('.').free,
+        'total_disk': disk_usage('.').total,
+        'network': {
+            'sent': sent,
+            'recv': recv,
+        },
+    }
+```
+
+---
 
 ## ***Cron Job Workflow***:
-- Format for Msg Update Interval
+- Format for Tg Message Edit/Update Interval
   - `*/5 * * * *`: Update Every 5mins Interval
+  > Due to Github Runner, Working Time Varies from 5min to more..
   - `0 */2 * * *`: Update Every 2hrs Interval
 
-## ***Credits***
+---
+
+## ***Credits***:
 - SilentDemonSD (Developer)
+
+## ***References***:
 - Based on `xditya/BotStatus`
+- Based on `junedkh/ mirror-bot-status`
+- Written in PyroFork Framework (Extended Pyrogram)
--- main.py
+++ main.py
@@ -2,37 +2,71 @@
 from asyncio import sleep
 from logging import basicConfig, INFO, getLogger
 from time import time
+from os import getenv, path as ospath 
 from datetime import datetime
 from itertools import zip_longest
 
 from pytz import utc, timezone
-from decouple import config
-from telethon import TelegramClient, functions
-from telethon.sessions import StringSession
+from dotenv import load_dotenv
+from requests import get as rget
+from pyrogram import Client
+from pyrogram.errors import FloodWait, MessageNotModified
+from pyrogram.raw.types import InputPeerChat
+from pyrogram.raw.functions.messages import GetHistory
 
 basicConfig(level=INFO, format="[%(levelname)s] %(asctime)s - %(message)s")
-log = getLogger("TgBotStatus")
+log = getLogger(__name__)
 
+if CONFIG_ENV_URL := getenv('CONFIG_ENV_URL'):
+    try:
+        res = rget(CONFIG_ENV_URL)
+        if res.status_code == 200:
+            log.info("Downloading .env from CONFIG_ENV_URL")
+            with open('.env', 'wb+') as f:
+                f.write(res.content)
+        else:
+            log.error(f"Failed to Download .env due to Error Code {res.status_code}")
+    except Exception as e:
+        log.error(f"CONFIG_ENV_URL: {e}")
+
+if CONFIG_JSON_URL := getenv('CONFIG_JSON_URL'):
+    try:
+        res = rget(CONFIG_JSON_URL)
+        if res.status_code == 200:
+            log.info("Downloading config.json from CONFIG_JSON_URL")
+            with open('config.json', 'wb+') as f:
+                f.write(res.content)
+        else:
+            log.error(f"Failed to download config.json due to Error Code {res.status_code}")
+    except Exception as e:
+        log.error(f"CONFIG_JSON_URL: {e}")
+
+load_dotenv('.env', override=True)
+
+APP_ID = int(getenv("APP_ID", 0))
+API_HASH = getenv("API_HASH")
+PYRO_SESSION = getenv('PYRO_SESSION')
+if PYRO_SESSION is None:
+    log.error('PYRO_SESSION is not set')
+    exit(1)
+if not ospath.exists('config.json'):
+    log.error("config.json not Found!")
+    exit(1)
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    SESSION = config("SESSION")
-    LIST_BOTS = config("BOTS")
-    LIST_HOSTS = config("HOSTS")
-    CHANNEL_ID = config("CHANNEL_ID", cast=int)
-    MESSAGE_ID = config("MESSAGE_ID", cast=int)
-    CHANNEL_NAME = config("CHANNEL_NAME", default="FZX Paradox")
-    TIME_ZONE = config("TIME_ZONE", default="Asia/Kolkata")
-except BaseException as ex:
-    log.info(ex)
+    config = json_loads(open('config.json', 'r').read())
+    bots = config['bots']
+    channels = config['channels']
+except:
+    LOGGER.error("Error: config.json is not valid")
     exit(1)
 
-BOTS = LIST_BOTS.split()
-HOSTS = LIST_HOSTS.split()
+HEADER_MSG = getenv("HEADER_MSG", "Telegram Bot Status:")
+FOOTER_MSG = getenv("FOOTER_MSG", "Join FZX Paradox at @FZXParadox")
+TIME_ZONE = getenv("TIME_ZONE", "Asia/Kolkata")
 
-log.info("Connecting BotClient")
+log.info("Connecting pyroBotClient")
 try:
-    client = TelegramClient(StringSession(SESSION), api_id=APP_ID, api_hash=API_HASH).start()
+    client = Client("TgBotStatus", api_id=APP_ID, api_hash=API_HASH, session_string=PYRO_SESSION)
 except BaseException as e:
     log.warning(e)
     exit(1)
@@ -45,73 +79,141 @@ def progress_bar(current, total):
     p_str = '●' * cFull
     p_str += '○' * (12 - cFull)
     return f"[{p_str}] {round(pct, 2)}%"
+    
+def get_readable_time(seconds):
+    periods = [('d', 86400), ('h', 3600), ('m', 60), ('s', 1)]
+    result = ''
+    for period_name, period_seconds in periods:
+        if seconds >= period_seconds:
+            period_value, seconds = divmod(seconds, period_seconds)
+            result += f'{int(period_value)}{period_name}'
+    return result
+    
+SIZE_UNITS   = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB']
+    
+def get_readable_file_size(size_in_bytes):
+    if size_in_bytes is None:
+        return '0B'
+    index = 0
+    while size_in_bytes >= 1024 and index < len(SIZE_UNITS) - 1:
+        size_in_bytes /= 1024
+        index += 1
+    return f'{size_in_bytes:.2f}{SIZE_UNITS[index]}' if index > 0 else f'{size_in_bytes}B'
+
+    
+async def bot_info(user_id):
+    try:
+        return (await client.get_users(user_id)).mention
+    except Exception:
+        return ''
+    
+async def editMsg(chat_id, message_id, text):
+    try:
+        return client.edit_message(chat_id, message_id, text)
+    except FloodWait as f:
+        await sleep(f.value * 1.2)
+        await editMsg(chat_id, message_id, text)
+    except MessageNotModified:
+        pass
+
+async def editStatusMsg(status_msg):
+    _channels = channels.values()
+    if len(_channels) == 0:
+        LOGGER.warning("No channels found")
+        exit(1)
+    for channel in _channels:
+        log.info(f"Updating {channel['chat_id']}: {channel['message_id']}")
+        await sleep(1.5)
+        try:
+            await editMsg(channel['chat_id'], channel['message_id'], status_msg)
+        except Exception as e:
+            log.error(str(e))
+            continue
 
 async def check_bots():
     start_time = time()
     bot_stats = {}
-    log.info("[CHECK] Started Periodic Bot Status checks...")
-    header_msg = f"__**{CHANNEL_NAME} Bot Status :**__\n\n"
+    totalBotsCount = len(bots.keys())
+    log.info("Starting Periodic Bot Status checks...")
+    
+    header_msg = f"__**{HEADER_MSG}**__\n\n"
     status_message = header_msg + """• **Avaliable Bots :** __Checking...__
 
 • `Currently Ongoing Periodic Check`
 
 """
-    try:
-        await client.edit_message(CHANNEL_ID, MESSAGE_ID, status_message + f"""**Status Update Stats:**
-┌ **Bots Verified :** 0 out of {len(BOTS)}
+    await editStatusMsg(status_message + f"""**Status Update Stats:**
+┌ **Bots Verified :** 0 out of {len(totalBotsCount)}
 ├ **Progress :** [○○○○○○○○○○] 0%
 └ **Time Elasped :** 0s""")
-    except BaseException as e:
-        log.warning("[EDIT] Unable to edit message in the channel!")
-        log.error(e)
-        return
 
     bot_no, avl_bots = 0, 0
-    for bot, host in zip_longest(BOTS, HOSTS):
-        if bot is None:
+    for bot, bdata in bots.items():
+        if not bot or not bdata:
             break
+        bot_stats.setdefault(bot, {})
+        bot_stats[bot]['host'] = bdata['host']
         pre_time = time()
+        if bdata.get('base_url_of_bot'):
+            resp = rget(f"{bdata['base_url_of_bot']}/status")
+            if resp.status_code == 200:
+                bot_stats[bot]["status_data"] = resp.json()
         try:
-            sent_msg = await client.send_message(bot, "/start")
+            sent_msg = await client.send_message(bdata['bot_uname'], "/start")
             await sleep(10)
-            history = await client(
-                functions.messages.GetHistoryRequest(
-                    peer=bot, offset_id=0, offset_date=None, add_offset=0, limit=1, max_id=0, min_id=0, hash=0,
+            msg_history = await client.invoke(
+                GetHistory(
+                    peer=client.resolve_peer(bdata['bot_uname']), offset_id=0, offset_date=None, add_offset=0, limit=1, max_id=0, min_id=0, hash=0,
                 )
             )
-            if sent_msg.id == history.messages[0].id:
-                bot_stats[bot] = {"response_time": None, "status": "❌", "host": host or "Unknown"}
+            if sent_msg.id == msg_history.id:
+                bot_stats[bot]["status"] = "❌"
             else:
-                resp_time = history.messages[0].date.timestamp() - pre_time
+                resp_time = abs(msg_history.date.timestamp() - pre_time)
                 avl_bots += 1
-                bot_stats[bot] = {"response_time": f"`{round(resp_time * 1000, 2)}ms`", "status": "✅", "host": host or "Unknown"}
+                bot_stats[bot]["response_time"] = f"`{round(resp_time * 1000, 2)}ms`"
+                bot_stats[bot]["status"] = "✅"
         except BaseException:
-            bot_stats[bot] = {"response_time": None, "status": "❌", "host": host or "Unknown"}
+            bot_stats[bot]["status"] = "❌"
         
-        await client.send_read_acknowledge(bot)
-        log.info(f"[CHECK] Checked @{bot} - {bot_stats[bot]['status']}.")
+        await client.read_chat_history(bdata['bot_uname'])
+        log.info(f"Checked @{bdata['bot_uname']} - {bot_stats[bot]['status']}.")
         bot_no += 1
         
-        await client.edit_message(CHANNEL_ID, MESSAGE_ID, status_message + f"""**Status Update Stats:**
-┌ **Bots Verified :** {bot_no} out of {len(BOTS)}
-├ **Progress :** {progress_bar(bot_no, len(BOTS))}
+        await editStatusMsg(status_message + f"""**Status Update Stats:**
+┌ **Bots Checked :** {bot_no} out of {len(totalBotsCount)}
+├ **Progress :** {progress_bar(bot_no, len(totalBotsCount))}
 └ **Time Elasped :** {round(time() - start_time, 2)}s""")
 
     end_time = time()
-    log.info("[CHECK] Completed periodic checks.")
+    log.info("Completed periodic checks.")
 
-    status_message = header_msg + f"• **Avaliable Bots :** {avl_bots} out of {len(BOTS)}\n\n"
-    for bot, value in bot_stats.items():
-        if bot_stats[bot]["response_time"] is None:
-            status_message += f"""┌ **Bot :** @{bot}
-├ **Status :** {bot_stats[bot]['status']}
-└ **Host :** {bot_stats[bot]['host']}
-            
-"""
-        else:
-            status_message += f"""┌ **Bot :** @{bot}
-├ **Ping :** {bot_stats[bot]['response_time']}
-├ **Status :** {bot_stats[bot]['status']}
+    status_message = header_msg + f"• **Avaliable Bots :** {avl_bots} out of {len(totalBotsCount)}\n\n"
+    for bot in bot_stats.keys():
+        status_message += f"┌ **Bot :** {(await bot_info(bdata['bot_uname'])} ( @{bdata['bot_uname']} )"
+        if (stdata := bot_stats[bot].get('status_data')):
+            try:
+                status_message += f'├ **Commit Date :** {stdata["commit_date"]}\n'
+            except:
+                pass
+            try:
+                status_message += f'├ **Bot Uptime :** {get_readable_time(stdata["on_time"])}\n'
+            except:
+                pass
+            try:
+                status_message += f'├ **Alive :** {get_readable_time(stdata["uptime"])}\n'
+            except:
+                pass
+            try:
+                status_message += f'├ **Up :** {get_readable_size(stdata["network"]["sent"])} '
+                status_message += f'| **DL :** {get_readable_size(stdata["network"]["recv"])}\n'
+                status_message += f'├ **Free Disk :** {get_readable_size(stdata["free_disk"])} / {get_readable_size(stdata["total_disk"])}\n'
+            except:
+                status_message += '├ **Something went Wrong!**'
+        
+        if bot_stats[bot].get("response_time"):
+            status_message += f"├ **Ping :** {bot_stats[bot]['response_time']}\n"
+        status_message += f"""├ **Status :** {bot_stats[bot]['status']}
 └ **Host :** {bot_stats[bot]['host']}
             
 """
@@ -125,14 +227,13 @@ async def check_bots():
 ├ **Date :** `{current_time.strftime('%d %B %Y')}`
 └ **Time Zone :** `{TIME_ZONE} (UTC {current_time.strftime('%z')})`
 
-__○ Auto Status Update in 5 mins Interval__"""
+__○ Auto Status Update in 5 mins Interval__
 
-    try:
-        await client.edit_message(CHANNEL_ID, MESSAGE_ID, status_message)
-    except BaseException as e:
-        log.warning("[EDIT] Unable to edit message in the channel!")
-        log.error(e)
-        return
+{FOOTER_MSG}"""
+    await editStatusMsg(status_message)
 
-
-client.loop.run_until_complete(check_bots())
+async def main():
+    async with client:
+        await check_bots()
+        
+client.run(main())
